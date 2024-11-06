94b3a10e36788cd1c2b9f63ce4a40cf8d6758c4b
Sebastiao Trindade <sebatrindade@hotmail.com>
2024-11-06 14:54:43 -0300

    Seção categorias

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,8 +1,6 @@
 class Category < ApplicationRecord
   validates :name, presence: true, uniqueness: true
-  validates :category_type, presence: true, inclusion: { in: ['Despesa', 'Receita']}
-  
-  
-  
+  validates :category_type, presence: true, inclusion: { in: ['Despesa', 'Receita']} 
+  enum category_type: { receita: 'receita', despesa: 'despesa'}
   
 end
